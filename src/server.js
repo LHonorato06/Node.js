3f53eea43739333c8076b93d03846687ecb67071
Lucas Honorato <honoratolucas2006@gmail.com>
2025-03-20 13:51:19 -0300

    corpo da requisição em JSON

--- src/server.js
+++ src/server.js
@@ -4,25 +4,39 @@ import http from "node:http";
 const users = []
 
 
-const server = http.createServer((request, response) => {
-  const { method, url } = request
+const server = http.createServer(async(req, res) => {
+  const { method, url } = req
+
+  const buffers = []
+  
+  for await (const chunk of req){
+    buffers.push(chunk)
+  }
+
+  try{
+  req.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch{
+    req.body = null
+  }
 
   if(method == 'GET' && url == '/users'){
-    return response
+    return res
     .setHeader('Content-type','application/json')
     .end(JSON.stringify(users))
     //'Listagem de usuários'
   }
 
   if (method == 'POST' && url == '/users'){
+    const {name, email} = req.body
+
     users.push({
       id: 1,
       name: 'Leroy',
       email: 'Carlitostevez@gmail.com',
     })
-    return response.writeHead(201).end()
+    return res.writeHead(201).end()
   }
-  return response.writeHead(404).end("Not Found");
+  return res.writeHead(404).end("Not Found");
 });
 
 server.listen(3333);
